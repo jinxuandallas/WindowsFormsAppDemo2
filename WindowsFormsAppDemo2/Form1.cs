f9a334925c5e35fe4e6e84ff7b9d9257876b07ee
Dallas <dallas2k@163.com>
2020-02-23 21:37:39 +0800

    初步完成拖拽

--- WindowsFormsAppDemo2/Form1.cs
+++ WindowsFormsAppDemo2/Form1.cs
@@ -10,9 +10,29 @@ using System.Windows.Forms;
 
 namespace WindowsFormsAppDemo2
 {
+
     public partial class Form1 : Form
     {
-        private bool mouseDown = false;
+        /// <summary>
+        /// 是否按下鼠标左键
+        /// </summary>
+        private bool cellMouseDown = false;
+        /// <summary>
+        /// 刚刚是否拖动过
+        /// </summary>
+        private bool justDrop = false;
+        /// <summary>
+        /// 拖动的第一个单元格
+        /// </summary>
+        private DataGridCell firstCell = new DataGridCell(-1, -1);
+        /// <summary>
+        /// 拖动的上一个经过的单元格
+        /// </summary>
+        private DataGridCell lastCell;
+        /// <summary>
+        /// 拖动到的行数（或列数）
+        /// </summary>
+        private int dropNumber = -1;
         public Form1()
         {
             InitializeComponent();
@@ -40,7 +60,53 @@ namespace WindowsFormsAppDemo2
         private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
         {
             DataGridView dgv = (DataGridView)sender;
-            if (e.RowIndex == dgv.CurrentCell.RowIndex && e.ColumnIndex == dgv.CurrentCell.ColumnIndex)
+
+            if (dropNumber != -1)
+            {
+                //dgv.ClearSelection();
+                if (dropNumber > firstCell.RowNumber)
+                {
+                    if (e.RowIndex >= firstCell.RowNumber && e.RowIndex <= dropNumber && e.ColumnIndex == firstCell.ColumnNumber)
+                    {
+                        if (e.RowIndex == firstCell.RowNumber)
+                        {
+                            e.Graphics.FillRectangle(new SolidBrush(Color.White), e.CellBounds);
+                            e.Graphics.DrawRectangle(new Pen(Color.Green, 2), new Rectangle(e.CellBounds.Left + 1, e.CellBounds.Top + 1, e.CellBounds.Width - 2, e.CellBounds.Height - 2));
+                            if (e.Value != null)
+                            {
+                                e.Graphics.DrawString((e.Value).ToString(), e.CellStyle.Font, new SolidBrush(Color.Black), e.CellBounds.X, e.CellBounds.Y + 5, StringFormat.GenericDefault);
+                            }
+                            e.Handled = true;
+                            return;
+                        }
+                        else
+                        {
+                            e.Graphics.FillRectangle(new SolidBrush(Color.White), e.CellBounds);
+                            //e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right, e.CellBounds.Bottom - 1);
+                            //e.Graphics.DrawRectangle(new Pen(Color.Green, 2), new Rectangle(e.CellBounds.Left + 1, e.CellBounds.Top + 1, e.CellBounds.Width - 2, e.CellBounds.Height - 2));
+                            e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left + 1, e.CellBounds.Bottom - 1, e.CellBounds.Left + 1, e.CellBounds.Top - 1);
+
+                            e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Right - 1, e.CellBounds.Top - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+
+                            //如果当前单元格是拖动到的最后一行
+                            if (e.RowIndex == dropNumber)
+                                e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+                            else
+                                e.Graphics.DrawLine(new Pen(Color.DarkGray, 1), e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+                            //e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left - 1, e.CellBounds.Top - 1, e.CellBounds.Right - 1, e.CellBounds.Top - 1);
+                            if (e.Value != null)
+                            {
+                                e.Graphics.DrawString((e.Value).ToString(), e.CellStyle.Font, new SolidBrush(Color.Black), e.CellBounds.X, e.CellBounds.Y + 5, StringFormat.GenericDefault);
+                            }
+                            //}
+                            e.Handled = true;
+                            return;
+                        }
+
+                    }
+                }
+            }
+            else if (e.RowIndex == dgv.CurrentCell.RowIndex && e.ColumnIndex == dgv.CurrentCell.ColumnIndex)
             {
                 ///*正式代码
                 Brush gridBrush = new SolidBrush(Color.Green);
@@ -60,7 +126,7 @@ namespace WindowsFormsAppDemo2
                 //}
                 e.Handled = true;
                 //*/
-                label1.Text = "row=" + dgv.CurrentCell.RowIndex + " column=" + dgv.CurrentCell.ColumnIndex;
+                //label1.Text = "row=" + dgv.CurrentCell.RowIndex + " column=" + dgv.CurrentCell.ColumnIndex;
             }
             //else
             //{
@@ -96,8 +162,17 @@ namespace WindowsFormsAppDemo2
         {
             if (e.Button == MouseButtons.Left)
             {
-                mouseDown = true;
-                label1.Text += "CellMouseDown  ";
+                cellMouseDown = true;
+
+                //按下鼠标，判断刚才是否拖动过，如果是则初始化相应参数，改变CellPainting行为
+                if (justDrop)
+                {
+                    justDrop = false;
+                    firstCell.RowNumber = -1;
+                    firstCell.ColumnNumber = -1;
+                    dropNumber = -1;
+                }
+                //label1.Text += "CellMouseDown  ";
                 label1.Text = ((DataGridView)sender).Cursor == Cursors.Cross ? "cross" : "default";
 
 
@@ -127,8 +202,16 @@ namespace WindowsFormsAppDemo2
         {
             if (e.Button == MouseButtons.Left)
             {
-                mouseDown = false;
+
                 label1.Text += "CellMouseUp  ";
+                //DataGridView dgv = (DataGridView)sender;
+                //if (firstCell.RowNumber != -1)
+                //    dgv.CurrentCell = dgv.Rows[firstCell.RowNumber].Cells[firstCell.ColumnNumber];
+                //恢复初始值
+                cellMouseDown = false;
+                //firstCell.RowNumber = -1;
+                //firstCell.ColumnNumber = -1;
+                //dropNumber = -1;
             }
         }
 
@@ -152,6 +235,9 @@ namespace WindowsFormsAppDemo2
             //messageBoxCS.AppendFormat("{0} = {1}", "Location", e.Location);
             //messageBoxCS.AppendLine();
             //MessageBox.Show(messageBoxCS.ToString(), "CellMouseMove Event");
+
+
+            //鼠标到当前单元格右下角时鼠标变成加号
             DataGridView dgv = (DataGridView)sender;
             if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex)
             {
@@ -190,15 +276,31 @@ namespace WindowsFormsAppDemo2
         private void dataGridView1_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
         {
             DataGridView dgv = (DataGridView)sender;
-            //判断鼠标离开的单元格是不是当前单元格，如果是则将鼠标图标改成默认
+            //判断鼠标离开的单元格是当前单元格，如果是则将鼠标图标改成默认
             if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex)
                 dgv.Cursor = Cursors.Default;
-            if (mouseDown)
-                label1.Text += "CellMouseLeave  ";
+            if (cellMouseDown)
+            {
+                justDrop = true;
+                if (firstCell.RowNumber == -1)
+                    firstCell = new DataGridCell(e.RowIndex, e.ColumnIndex);
+                lastCell = new DataGridCell(e.RowIndex, e.ColumnIndex);
+                //label1.Text += "leave row:" + e.RowIndex + " column:" + e.ColumnIndex;
+            }
         }
 
         private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
         {
+            if (cellMouseDown)
+            {
+                if (dropNumber == -1)
+                    dropNumber = firstCell.RowNumber;
+                if (e.RowIndex != lastCell.RowNumber)
+                    dropNumber = e.RowIndex > lastCell.RowNumber ? dropNumber + 1 : dropNumber - 1;
+                //label1.Text += "\n enter row:" + e.RowIndex + " column:" + e.ColumnIndex;
+                //label1.Text += "\n leave row:" + firstCell.RowNumber + " column:" + firstCell.ColumnNumber;
+                label1.Text += "\n first:" + firstCell.RowNumber + " drop:" + dropNumber;
+            }
             //失败，只能在进入单元格时判断，不符合要求
             //DataGridView dgv = (DataGridView)sender;
             //if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex&& MouseinCorner(dgv))
