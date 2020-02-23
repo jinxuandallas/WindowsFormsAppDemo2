1a242077713714ec2605ef1ee9ef581af4e2e217
Dallas <dallas2k@163.com>
2020-02-23 17:50:51 +0800

    添加项目文件。

---
+++ WindowsFormsAppDemo2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsAppDemo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(648, 329);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
+            this.dataGridView1.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
+            this.dataGridView1.CellMouseLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseLeave);
+            this.dataGridView1.CellMouseMove += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseMove);
+            this.dataGridView1.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseUp);
+            this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 385);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsAppDemo2/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppDemo2
+{
+    public partial class Form1 : Form
+    {
+        private bool mouseDown = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+            DataTable dt = new DataTable();
+            for (int i = 0; i < 5; i++)
+                dt.Columns.Add();
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                DataRow dr = dt.NewRow();
+                for (int j = 0; j < 5; j++)
+                    dr[j] = rnd.Next(100);
+                dt.Rows.Add(dr);
+            }
+            dataGridView1.DataSource = dt;
+        }
+
+        //private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        //{
+        //    DataGridView dgv = (DataGridView)sender;
+        //    //dgv[e.ColumnIndex,e.RowIndex].Style.
+        //}
+
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            DataGridView dgv = (DataGridView)sender;
+            if (e.RowIndex == dgv.CurrentCell.RowIndex && e.ColumnIndex == dgv.CurrentCell.ColumnIndex)
+            {
+                ///*正式代码
+                Brush gridBrush = new SolidBrush(Color.Green);
+                //using (Pen gridLinePen = new Pen(gridBrush))
+                //{
+                e.Graphics.FillRectangle(new SolidBrush(Color.White), e.CellBounds);
+                //e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right, e.CellBounds.Bottom - 1);
+                e.Graphics.DrawRectangle(new Pen(Color.Green, 2), new Rectangle(e.CellBounds.Left + 1, e.CellBounds.Top + 1, e.CellBounds.Width - 2, e.CellBounds.Height - 2));
+                //e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left - 1, e.CellBounds.Bottom - 1, e.CellBounds.Left - 1, e.CellBounds.Top - 1);
+                //e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left - 1, e.CellBounds.Bottom - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+                //e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Right - 1, e.CellBounds.Top - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+                //e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left - 1, e.CellBounds.Top - 1, e.CellBounds.Right - 1, e.CellBounds.Top - 1);
+                if (e.Value != null)
+                {
+                    e.Graphics.DrawString((e.Value).ToString(), e.CellStyle.Font, new SolidBrush(Color.Black), e.CellBounds.X, e.CellBounds.Y + 5, StringFormat.GenericDefault);
+                }
+                //}
+                e.Handled = true;
+                //*/
+                label1.Text = "row=" + dgv.CurrentCell.RowIndex + " column=" + dgv.CurrentCell.ColumnIndex;
+            }
+            //else
+            //{
+            //    Brush gridBrush = new SolidBrush(Color.Green);
+            //    using (Pen gridLinePen = new Pen(gridBrush))
+            //    {
+            //        e.Graphics.FillRectangle(new SolidBrush(Color.White), e.CellBounds);
+            //        //e.Graphics.DrawLine(gridLinePen, e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right, e.CellBounds.Bottom - 1);
+            //        e.Graphics.DrawLine(new Pen(Color.Black, 2), e.CellBounds.Left, e.CellBounds.Bottom, e.CellBounds.Left, e.CellBounds.Top);
+            //        e.Graphics.DrawLine(new Pen(Color.Black, 2), e.CellBounds.Left, e.CellBounds.Bottom, e.CellBounds.Right, e.CellBounds.Bottom);
+            //        e.Graphics.DrawLine(new Pen(Color.Black, 2), e.CellBounds.Right, e.CellBounds.Top, e.CellBounds.Right, e.CellBounds.Bottom);
+            //        e.Graphics.DrawLine(new Pen(Color.Black, 2), e.CellBounds.Left, e.CellBounds.Top, e.CellBounds.Right, e.CellBounds.Top);
+            //        if (e.Value != null)
+            //        {
+            //            e.Graphics.DrawString((e.Value).ToString(), e.CellStyle.Font, new SolidBrush(Color.Black), e.CellBounds.X + 2, e.CellBounds.Y + 5, StringFormat.GenericDefault);
+            //        }
+            //    }
+            //    e.Handled = true;
+            //}
+        }
+
+        //private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        //{
+        //    label1.Text = "DragDrop";
+        //}
+
+        //private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        //{
+
+        //}
+
+        private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDown = true;
+                label1.Text += "CellMouseDown  ";
+                label1.Text = ((DataGridView)sender).Cursor == Cursors.Cross ? "cross" : "default";
+
+
+
+                //System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+                //messageBoxCS.AppendFormat("{0} = {1}", "ColumnIndex", e.ColumnIndex);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "RowIndex", e.RowIndex);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Button", e.Button);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Clicks", e.Clicks);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "X", e.X);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Y", e.Y);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Delta", e.Delta);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Location", e.Location);
+                //messageBoxCS.AppendLine();
+                //MessageBox.Show(messageBoxCS.ToString(), "CellMouseDown Event");
+            }
+        }
+
+        private void dataGridView1_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseDown = false;
+                label1.Text += "CellMouseUp  ";
+            }
+        }
+
+        private void dataGridView1_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            //System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            //messageBoxCS.AppendFormat("{0} = {1}", "ColumnIndex", e.ColumnIndex);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "RowIndex", e.RowIndex);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "Button", e.Button);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "Clicks", e.Clicks);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "X", e.X);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "Y", e.Y);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "Delta", e.Delta);
+            //messageBoxCS.AppendLine();
+            //messageBoxCS.AppendFormat("{0} = {1}", "Location", e.Location);
+            //messageBoxCS.AppendLine();
+            //MessageBox.Show(messageBoxCS.ToString(), "CellMouseMove Event");
+            DataGridView dgv = (DataGridView)sender;
+            if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex)
+            {
+                //System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+                //messageBoxCS.AppendFormat("{0} = {1}", "ColumnIndex", e.ColumnIndex);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "RowIndex", e.RowIndex);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Button", e.Button);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Clicks", e.Clicks);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "X", e.X);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Y", e.Y);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Delta", e.Delta);
+                //messageBoxCS.AppendLine();
+                //messageBoxCS.AppendFormat("{0} = {1}", "Location", e.Location);
+                //messageBoxCS.AppendLine();
+                //MessageBox.Show(messageBoxCS.ToString(), "CellMouseMove Event");
+                //label1.Text = messageBoxCS.ToString();
+
+                ///*
+                if (dgv.Cursor == Cursors.Default && MouseinCorner(dgv, e.X, e.Y))
+                    dgv.Cursor = Cursors.Cross;
+                if (dgv.Cursor == Cursors.Cross && !MouseinCorner(dgv, e.X, e.Y))
+                    dgv.Cursor = Cursors.Default;
+                //*/
+            }
+
+            //if (mouseDown)
+            //label1.Text += "CellMouseMove  ";
+        }
+
+        private void dataGridView1_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridView dgv = (DataGridView)sender;
+            //判断鼠标离开的单元格是不是当前单元格，如果是则将鼠标图标改成默认
+            if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex)
+                dgv.Cursor = Cursors.Default;
+            if (mouseDown)
+                label1.Text += "CellMouseLeave  ";
+        }
+
+        private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            //失败，只能在进入单元格时判断，不符合要求
+            //DataGridView dgv = (DataGridView)sender;
+            //if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex&& MouseinCorner(dgv))
+            //    dgv.Cursor = Cursors.Cross;
+        }
+
+        //private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        //{
+        //    label1.Text += "DragEnter\t";
+        //}
+
+        /// <summary>
+        /// 判断鼠标是否在单元格右下角，以便进入拖拽模式
+        /// </summary>
+        /// <param name="dgv"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private bool MouseinCorner(DataGridView dgv, int x, int y)
+        {
+            //因x,y,r皆是单元格内相对坐标，所以无需进行转换，直接进行判断
+            Rectangle r = dgv.GetCellDisplayRectangle(dgv.CurrentCell.ColumnIndex, dgv.CurrentCell.RowIndex, false);
+
+            //Point p = dgv.PointToClient(Form1.MousePosition);
+            //Point p = new Point(x, y);
+            return x > r.Width - 5 && y > r.Height - 5;
+        }
+    }
+}
