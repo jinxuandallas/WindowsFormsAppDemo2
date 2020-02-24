8852c44ec076ffa696a1c16b5c310997c5b64ecc
Dallas <dallas2k@163.com>
2020-02-24 12:08:35 +0800

    第一版程序已经改完

--- WindowsFormsAppDemo2/Form1.Designer.cs
+++ WindowsFormsAppDemo2/Form1.Designer.cs
@@ -31,6 +31,8 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
+            this.buttonUndo = new System.Windows.Forms.Button();
+            this.buttonRedo = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,14 +51,16 @@
             this.dataGridView1.Location = new System.Drawing.Point(25, 31);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(648, 329);
+            this.dataGridView1.Size = new System.Drawing.Size(500, 300);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridView1_CellBeginEdit);
             this.dataGridView1.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseDown);
             this.dataGridView1.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
             this.dataGridView1.CellMouseLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseLeave);
             this.dataGridView1.CellMouseMove += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseMove);
             this.dataGridView1.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseUp);
             this.dataGridView1.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.dataGridView1_CellPainting);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
             // 
             // label1
             // 
@@ -67,11 +71,35 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
+            // buttonUndo
+            // 
+            this.buttonUndo.Enabled = false;
+            this.buttonUndo.Location = new System.Drawing.Point(697, 70);
+            this.buttonUndo.Name = "buttonUndo";
+            this.buttonUndo.Size = new System.Drawing.Size(75, 23);
+            this.buttonUndo.TabIndex = 2;
+            this.buttonUndo.Text = "撤销";
+            this.buttonUndo.UseVisualStyleBackColor = true;
+            this.buttonUndo.Click += new System.EventHandler(this.buttonUndo_Click);
+            // 
+            // buttonRedo
+            // 
+            this.buttonRedo.Enabled = false;
+            this.buttonRedo.Location = new System.Drawing.Point(697, 114);
+            this.buttonRedo.Name = "buttonRedo";
+            this.buttonRedo.Size = new System.Drawing.Size(75, 23);
+            this.buttonRedo.TabIndex = 2;
+            this.buttonRedo.Text = "重做";
+            this.buttonRedo.UseVisualStyleBackColor = true;
+            this.buttonRedo.Click += new System.EventHandler(this.buttonRedo_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRedo);
+            this.Controls.Add(this.buttonUndo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -86,6 +114,8 @@
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonUndo;
+        private System.Windows.Forms.Button buttonRedo;
     }
 }
 
--- WindowsFormsAppDemo2/Form1.cs
+++ WindowsFormsAppDemo2/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace WindowsFormsAppDemo2
 {
-
+   
     public partial class Form1 : Form
     {
         /// <summary>
@@ -24,27 +24,29 @@ namespace WindowsFormsAppDemo2
         /// <summary>
         /// 拖动的第一个单元格
         /// </summary>
-        private DataGridCell firstCell = new DataGridCell(-1, -1);
+        private DataGridViewCell firstCell = null;
         /// <summary>
         /// 拖动的上一个经过的单元格
         /// </summary>
-        private DataGridCell lastCell;
+        private DataGridViewCell lastCell;
         /// <summary>
-        /// 拖动到的行数（或列数）
+        /// 拖动到的行数（或列数）***改进：dropNumber应改成正负数表示的和firstCell之间的距离
         /// </summary>
         private int dropNumber = -1;
+        private Stack<Step> undo = new Stack<Step>();
+        private Stack<Step> redo = new Stack<Step>();
         public Form1()
         {
             InitializeComponent();
 
             DataTable dt = new DataTable();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 10; i++)
                 dt.Columns.Add();
             Random rnd = new Random();
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 20; i++)
             {
                 DataRow dr = dt.NewRow();
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 10; j++)
                     dr[j] = rnd.Next(100);
                 dt.Rows.Add(dr);
             }
@@ -60,15 +62,15 @@ namespace WindowsFormsAppDemo2
         private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
         {
             DataGridView dgv = (DataGridView)sender;
-
-            if (dropNumber != -1)
+            //如果进行了拖动
+            if (justDrop)
             {
                 //dgv.ClearSelection();
-                if (dropNumber > firstCell.RowNumber)
+                if (dropNumber > firstCell.RowIndex)//如果向下拖动
                 {
-                    if (e.RowIndex >= firstCell.RowNumber && e.RowIndex <= dropNumber && e.ColumnIndex == firstCell.ColumnNumber)
+                    if (e.RowIndex >= firstCell.RowIndex && e.RowIndex <= dropNumber && e.ColumnIndex == firstCell.ColumnIndex)
                     {
-                        if (e.RowIndex == firstCell.RowNumber)
+                        if (e.RowIndex == firstCell.RowIndex)
                         {
                             e.Graphics.FillRectangle(new SolidBrush(Color.White), e.CellBounds);
                             e.Graphics.DrawRectangle(new Pen(Color.Green, 2), new Rectangle(e.CellBounds.Left + 1, e.CellBounds.Top + 1, e.CellBounds.Width - 2, e.CellBounds.Height - 2));
@@ -92,7 +94,7 @@ namespace WindowsFormsAppDemo2
                             if (e.RowIndex == dropNumber)
                                 e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
                             else
-                                e.Graphics.DrawLine(new Pen(Color.DarkGray, 1), e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
+                                e.Graphics.DrawLine(new Pen(SystemColors.ControlDark, 1), e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Right - 1, e.CellBounds.Bottom - 1);
                             //e.Graphics.DrawLine(new Pen(Color.Green, 2), e.CellBounds.Left - 1, e.CellBounds.Top - 1, e.CellBounds.Right - 1, e.CellBounds.Top - 1);
                             if (e.Value != null)
                             {
@@ -157,7 +159,12 @@ namespace WindowsFormsAppDemo2
         //{
 
         //}
-
+       private void initDrop()
+        {
+            justDrop = false;
+            firstCell = null;
+            dropNumber = -1;
+        }
         private void dataGridView1_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
@@ -166,12 +173,7 @@ namespace WindowsFormsAppDemo2
 
                 //按下鼠标，判断刚才是否拖动过，如果是则初始化相应参数，改变CellPainting行为
                 if (justDrop)
-                {
-                    justDrop = false;
-                    firstCell.RowNumber = -1;
-                    firstCell.ColumnNumber = -1;
-                    dropNumber = -1;
-                }
+                    initDrop();
                 //label1.Text += "CellMouseDown  ";
                 label1.Text = ((DataGridView)sender).Cursor == Cursors.Cross ? "cross" : "default";
 
@@ -205,13 +207,35 @@ namespace WindowsFormsAppDemo2
 
                 label1.Text += "CellMouseUp  ";
                 //DataGridView dgv = (DataGridView)sender;
-                //if (firstCell.RowNumber != -1)
-                //    dgv.CurrentCell = dgv.Rows[firstCell.RowNumber].Cells[firstCell.ColumnNumber];
+                //if (firstCell.RowIndex != -1)
+                //    dgv.CurrentCell = dgv.Rows[firstCell.RowIndex].Cells[firstCell.ColumnNumber];
                 //恢复初始值
                 cellMouseDown = false;
-                //firstCell.RowNumber = -1;
-                //firstCell.ColumnNumber = -1;
-                //dropNumber = -1;
+
+                if (justDrop && dropNumber != firstCell.RowIndex)
+                {
+                    DataGridView dgv = (DataGridView)sender;
+
+                    if (undo.Count == 0)
+                        buttonUndo.Enabled = true;
+
+                    if (dropNumber > firstCell.RowIndex)
+                        for (int i = firstCell.RowIndex + 1; i < dropNumber + 1; i++)
+                        {
+                            DataGridViewCell dgvc = dgv.Rows[i].Cells[firstCell.ColumnIndex];
+                            undo.Push(new Step(i, firstCell.ColumnIndex, dgvc.Value));
+                            dgvc.Value = firstCell.Value;
+                            
+                        }
+                    else
+                        for (int i = dropNumber; i < firstCell.RowIndex; i++)
+                        {
+                            DataGridViewCell dgvc = dgv.Rows[i].Cells[firstCell.ColumnIndex];
+                            undo.Push(new Step(i, firstCell.ColumnIndex, dgvc.Value));
+                            dgvc.Value = firstCell.Value;
+                        }
+
+                }
             }
         }
 
@@ -264,7 +288,9 @@ namespace WindowsFormsAppDemo2
                 ///*
                 if (dgv.Cursor == Cursors.Default && MouseinCorner(dgv, e.X, e.Y))
                     dgv.Cursor = Cursors.Cross;
-                if (dgv.Cursor == Cursors.Cross && !MouseinCorner(dgv, e.X, e.Y))
+
+                //拖动期间超出范围不算退出拖拽
+                if (!cellMouseDown && dgv.Cursor == Cursors.Cross && !MouseinCorner(dgv, e.X, e.Y))
                     dgv.Cursor = Cursors.Default;
                 //*/
             }
@@ -277,29 +303,29 @@ namespace WindowsFormsAppDemo2
         {
             DataGridView dgv = (DataGridView)sender;
             //判断鼠标离开的单元格是当前单元格，如果是则将鼠标图标改成默认
-            if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex)
-                dgv.Cursor = Cursors.Default;
-            if (cellMouseDown)
+            //if (e.ColumnIndex == dgv.CurrentCell.ColumnIndex && e.RowIndex == dgv.CurrentCell.RowIndex)
+            //    dgv.Cursor = Cursors.Default;
+            if (cellMouseDown&& dgv.Cursor==Cursors.Cross)
             {
                 justDrop = true;
-                if (firstCell.RowNumber == -1)
-                    firstCell = new DataGridCell(e.RowIndex, e.ColumnIndex);
-                lastCell = new DataGridCell(e.RowIndex, e.ColumnIndex);
+                if (firstCell == null)
+                    firstCell = dgv.Rows[e.RowIndex].Cells[e.ColumnIndex];
+                lastCell = dgv.Rows[e.RowIndex<0?0:e.RowIndex].Cells[e.ColumnIndex < 0 ? 0:e.ColumnIndex] ;
                 //label1.Text += "leave row:" + e.RowIndex + " column:" + e.ColumnIndex;
             }
         }
 
         private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
         {
-            if (cellMouseDown)
+            if (cellMouseDown&& ((DataGridView)sender).Cursor==Cursors.Cross)
             {
                 if (dropNumber == -1)
-                    dropNumber = firstCell.RowNumber;
-                if (e.RowIndex != lastCell.RowNumber)
-                    dropNumber = e.RowIndex > lastCell.RowNumber ? dropNumber + 1 : dropNumber - 1;
+                    dropNumber = firstCell.RowIndex;
+                if (e.RowIndex != lastCell.RowIndex)
+                    dropNumber = e.RowIndex > lastCell.RowIndex ? dropNumber + 1 : dropNumber - 1;
                 //label1.Text += "\n enter row:" + e.RowIndex + " column:" + e.ColumnIndex;
-                //label1.Text += "\n leave row:" + firstCell.RowNumber + " column:" + firstCell.ColumnNumber;
-                label1.Text += "\n first:" + firstCell.RowNumber + " drop:" + dropNumber;
+                //label1.Text += "\n leave row:" + firstCell.RowIndex + " column:" + firstCell.ColumnNumber;
+                label1.Text += "\n first:" + firstCell.RowIndex + " drop:" + dropNumber;
             }
             //失败，只能在进入单元格时判断，不符合要求
             //DataGridView dgv = (DataGridView)sender;
@@ -326,7 +352,72 @@ namespace WindowsFormsAppDemo2
 
             //Point p = dgv.PointToClient(Form1.MousePosition);
             //Point p = new Point(x, y);
-            return x > r.Width - 5 && y > r.Height - 5;
+            return x > r.Width - 10 && y > r.Height - 10;
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            label1.Text = ((DataGridView)sender).Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString() ;
+        }
+
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            if (undo.Count == 0)
+                buttonUndo.Enabled = true;
+            undo.Push(new Step(e.RowIndex, e.ColumnIndex, ((DataGridView)sender).Rows[e.RowIndex].Cells[e.ColumnIndex].Value));
+        }
+
+        private void buttonUndo_Click(object sender, EventArgs e)
+        {
+            initDrop();
+
+            Step step = undo.Pop();
+            DataGridViewCell dgvc = dataGridView1.Rows[step.RowIndex].Cells[step.ColumnIndex];
+            object redoValue = dgvc.Value;
+            dgvc.Value = step.Value;
+
+            dataGridView1.CurrentCell=null;
+            dataGridView1.CurrentCell = dgvc;
+            if (undo.Count == 0)
+                buttonUndo.Enabled = false;
+
+            if (redo.Count == 0)
+                buttonRedo.Enabled = true;
+            step.Value = redoValue;
+            redo.Push(step);
+        }
+
+        private void buttonRedo_Click(object sender, EventArgs e)
+        {
+            initDrop();
+
+            Step step = redo.Pop();
+            DataGridViewCell dgvc = dataGridView1.Rows[step.RowIndex].Cells[step.ColumnIndex];
+            object undoValue = dgvc.Value;
+            dgvc.Value = step.Value;
+
+            dataGridView1.CurrentCell = null;
+            dataGridView1.CurrentCell = dgvc;
+            if (redo.Count == 0)
+                buttonRedo.Enabled = false;
+
+            if (undo.Count == 0)
+                buttonUndo.Enabled = true;
+            step.Value = undoValue;
+            undo.Push(step);
+        }
+
+    }
+    public struct Step
+    {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
+        public object Value { get; set; }
+        public Step(int rowIndex,int columnIndex,object value)
+        {
+            RowIndex = rowIndex;
+            ColumnIndex = columnIndex;
+            Value = value;
         }
     }
 }
